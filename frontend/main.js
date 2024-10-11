8ebd0fd213d9d7826a191d8747c50d642f9a9695
luis ramirez <2023169@student.cct.ie>
2024-10-11 16:29:01 +0100

    update the structure, cereate main.js file

---
+++ frontend/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded',(event) => { //When the content is loaded go and run this event
+    getVisitCount();
+})
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30; // This variable is has a value but only for the begining
+
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+
+        console.log("Website called function API.");
+        count = response.count; // the count variable are going to store the value that json response 
+        document.getElementById("counter").innetText = count; // Then go into thge document, find the counter ID , take the innertext and change by the new value from the response
+
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+
+}
